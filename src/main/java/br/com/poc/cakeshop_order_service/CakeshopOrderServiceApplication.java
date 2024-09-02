986f0218a24fafa3f7e381db4d694794de6fca59
alberto <alberto.henrique.sousa@gmail.com>
2024-09-02 14:43:03 -0300

    Start Order App

---
+++ src/main/java/br/com/poc/cakeshop_order_service/CakeshopOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.cakeshop_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeshopOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeshopOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/poc/cakeshop_order_service/CakeshopOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.poc.cakeshop_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeshopOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
